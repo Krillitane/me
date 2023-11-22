40a056f27740d3df324568147380f4d01d35ab30
Ronn <ligalahh@gmail.com>
2023-11-22 11:52:13 +0000

    testimonial update

--- main.js
+++ main.js
@@ -49,3 +49,39 @@ document.addEventListener("DOMContentLoaded", function () {
         },
     });
 });
+
+
+/* Swiper Testimonial */
+let swiperTestimonial = new Swiper(".testimonial-container", {
+    grabCursor: true,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+/* Email JS */
+const contactForm = document.getElementById('.contact-form'),
+      contactName = document.getElementById('.contact-name'),
+      contactEmail = document.getElementById('.contact-email'),
+      contactProject = document.getElementById('.contact-project'),
+      contactMessage = document.getElementById('.contact-message')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    //check if the field has value
+    if(contactName.value === '' || contactEmail.value === '' || contactProject.value === ''){
+        //Add and remove color
+        contactMessage.classList.remove('color-blue')
+        contactMessage.add('color-red')
+
+        //show message
+        contactMessage.textContent = 'Write all the input fields 📩'
+    }else{
+        // serviceID - templateID - #form - publicKey
+        emailjs.sendForm('','','','')
+    }
+}
+contactForm.addEventListener('submit', sendEmail)
